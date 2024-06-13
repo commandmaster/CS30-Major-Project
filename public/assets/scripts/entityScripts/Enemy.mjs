cee237d588d46714a8c6b7e77a03be637509dd6b
commandmaster <153877155+commandmaster@users.noreply.github.com>
2024-06-12 22:44:20 -0600

    started the enemy script

---
+++ public/assets/scripts/entityScripts/Enemy.mjs
@@ -0,0 +1,23 @@
+import { ScriptingAPI } from "../../../FrontendModules/scriptingModule.mjs";
+import { EntityAPI } from "../../../FrontendModules/entityModule.mjs";
+import { MathPlus } from "../../../SharedCode/mathPlus.mjs";
+import { Vec2 } from "../../../SharedCode/physicsEngine.mjs";
+const Physics = ScriptingAPI.Physics;
+
+
+export default class Enemy extends ScriptingAPI.Monobehaviour {
+    constructor(engineAPI, entity) {
+        super(engineAPI, entity);
+    }
+
+    Start() {
+        
+    }
+
+    Update(dt) {
+        
+    }
+   
+}
+
+
